e5d340f68dd92c8859f65ab80c907e9e3a7787ca
canertasan <caner@feedbackfruits.com>
2023-03-30 02:01:02 +0200

    Add home page & login / sign-up flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'reservations/index'
-  get 'reservations/new'
-  get 'reservations/create'
-  get 'reservations/show'
+  root 'home#index'
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
